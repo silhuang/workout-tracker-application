e6d7046269b5681c9e7d2a6e124e5a2e717faf94
silhuang <silvanahuang23@gmail.com>
2022-10-27 12:49:17 -0700

    - Fixed the viewTracks() methodlength problem by dividing the respective tasks into different methods - Added two user stores related to data persistence

--- README.md
+++ README.md
@@ -3,34 +3,31 @@
 ## Keep all your Pilates & workout choreo in one single app! 
 
 
-The **Pilates Workout Tracker Application** will allow users to keep track of their workouts on a single app. 
+The **Pilates Workout Tracker Application** will allow users to keep track of their favourite workout on a single app. 
 When users create a new workout, they can add as many tracks as they wish to the workout. 
 **Tracks** contain **different moves in a certain order (choreography)**, where each **move** is associated with a 
 **name** and **number of reps**. Originally, this app was intended for Pilates instructors to record the choreography 
-of every track in each of their workouts in one place, but it can also be used by anyone who would like to 
-record their workouts in a single application. _This project is of interest to me because I was previously a 
+of every track their chosen workout in one place, but it can also be used by anyone who would like to 
+record their workout in a single application. _This project is of interest to me because I was previously a 
 POP Pilates instructor with the UBC Pilates Club_. As I learned different POP choreographies (i.e. workouts), 
 I wrote down the moves of every track inside a notebook, but thought it would be more convenient to store 
-the information I recorded inside an app that I can access anytime. 
+the information I recorded inside an app that I can access anytime.
 
 User Stories:
 
-- As a user, I want to be able to add an arbitrary number of workouts to my tracker
 - As a user, I can add as many tracks as I want to a workout
 - As a user, I can add as many moves as I want to any given track
-- As a user, I can delete any previously added 
-  - workouts from my tracker
+- As a user, I can delete any previously added
   - tracks from a workout
   - moves from a track
   
-- As a user, I want to be able to view all the tracks recorded so far in any workout
+- As a user, I want to be able to view all the tracks recorded so far my workout
 - As a user, I want to be able to view the order and reps of moves in any track
 
-_Additional User Stories if Time Permits:_
-- As a user, I want to be able to duplicate a previously created track/move as many times as I wish
-- As a user, I want to be able to change the order of workouts, tracks, or moves
-- As a user, I want to be able to add previously created tracks from pre-existing workouts to new workouts 
-
-
+**Data Persistence**
+- As a user, when I select "Quit application" from the main menu, I want to be given the option of 
+  whether I would like to save my newly created workout to file
+- As a user, when I start the application, I have the option of loading my previous workout from file
+  by clicking "View saved workout" from the main menu
 
 
--- src/main/ui/WorkoutTrackerApp.java
+++ src/main/ui/WorkoutTrackerApp.java
@@ -1,10 +1,10 @@
 package ui;
 
+import exceptions.UnrealisticRepsException;
 import model.Move;
 import model.Track;
 import model.Workout;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 // Workout tracker application
@@ -14,7 +14,7 @@ import java.util.Scanner;
 // Pilates Workout Tracker Application
 public class WorkoutTrackerApp {
     private Scanner userInput = new Scanner(System.in);
-    private ArrayList<Workout> workouts = new ArrayList<>();
+    private Workout workout;
 
 
     // EFFECTS: runs the workout tracker application
@@ -47,7 +47,7 @@ public class WorkoutTrackerApp {
     private void showMainMenu() {
         System.out.println("\nPlease select one of the following options:");
         System.out.println("\t1. Create a new workout");
-        System.out.println("\t2. View saved workouts");
+        System.out.println("\t2. View saved workout");
         System.out.println("\t3. Quit application");
     }
 
@@ -55,30 +55,174 @@ public class WorkoutTrackerApp {
     // EFFECTS: processes user entry
     private void processEntry(int userEntry) {
         if (userEntry == 1) {
-            createNewWorkout();
+            createWorkout();
         } else if (userEntry == 2) {
-            //viewWorkouts();
+            viewSavedWorkout();
         } else {
             System.out.println("Invalid selection" + "\nPlease try again");
         }
     }
 
-    private void createNewWorkout() {
-        System.out.println("Please enter the title of your new workout:");
-        String titleOfNewWorkout = userInput.next();
-        for (Workout w : workouts) {
-            String existingTitle = w.getWorkoutTitle();
-            if (existingTitle.equals(titleOfNewWorkout)) {
-                System.out.println("Sorry, the title you have entered has already been used."
-                         + "Please enter a different title:");
-                //createNewWorkout();
-            }
-            Workout newWorkout = new Workout(titleOfNewWorkout);
-            workouts.add(newWorkout);
-            System.out.println("Your workout " + "\"" + titleOfNewWorkout + "\"" + " has been created!");
+    // EFFECTS: prints out the tracks of the previously saved workout
+    private void viewSavedWorkout() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: creates a new workout with a name of the user's choice
+    private void createWorkout() {
+        System.out.println("Please enter the tile of your new workout:");
+        String workoutTitle = userInput.next();
+        workout = new Workout(workoutTitle);
+        System.out.println("Your workout " + "\"" + workoutTitle + "\"" + " has been created");
+        nextOptionsAfterWorkout();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: shows options after workout is created and processes subsequent entry
+    private void nextOptionsAfterWorkout() {
+        System.out.println("Please select your next option:");
+        System.out.println("\t 1. Add new track");
+        System.out.println("\t 2. View tracks");
+        System.out.println("\t 3. Return to main menu");
+
+        int userEntry = userInput.nextInt();
+
+        if (userEntry == 1) {
+            addTrack();
+        } else if (userEntry == 2) {
+            viewTracks();
+        } else {
+            //showMainMenu();
         }
+    }
 
+    // MODIFIES: this
+    // EFFECTS: adds the newly created track to the current workout
+    private void addTrack() {
+        System.out.println("Please enter the title of your new track:");
+        String newTrackTitle = userInput.next();
+        for (Track t : workout) {
+            String existingTrackTitle = t.getTrackTitle();
+            if (existingTrackTitle.equals(newTrackTitle)) {
+                System.out.println("Sorry, the title you entered has already been used.");
+                addTrack();
+            }
+        }
+        Track newTrack = new Track(newTrackTitle);
+        workout.addTrack(newTrack);
+        System.out.println("\"" + newTrackTitle + "\"" + " has been added to the workout "
+                + "\"" + workout.getWorkoutTitle() + "\"!");
+        nextOptionsAfterWorkout();
+    }
 
+    // EFFECTS: prints out the name of all tracks added the workout so far in the order they were added
+    private void viewTracks() {
+        for (Track t : workout) {
+            String trackName = t.getTrackTitle();
+            int position = workout.getTracks().indexOf(t);
+            position++;
+            System.out.println(position + ". " + trackName);
+        }
+        afterViewTracks();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: displays available options after tracks are viewed, processes subsequent user entry
+    public void afterViewTracks() {
+        System.out.println("Please select one of the following options:");
+        System.out.println("\t 1. Select a track to view");
+        System.out.println("\t 2. Delete a track");
+        System.out.println("\t 3. Return to main menu");
+
+        int userEntry = userInput.nextInt();
+
+        if (userEntry == 1) {
+            getTrackToViewFromUserInput();
+        } else if (userEntry == 2) {
+            deleteTrack();
+        } else {
+            //showMainMenu();
+        }
+    }
+
+    private void getTrackToViewFromUserInput() {
+        System.out.println("Please enter the number corresponding to the track you would like to view:");
+        int trackNumber = userInput.nextInt();
+        trackNumber--;
+        Track trackToView = workout.getTracks().get(trackNumber);
+        printMovesInTrack(trackToView);
+    }
+
+    private void printMovesInTrack(Track trackToView) {
+        if (trackToView.getMoves().isEmpty()) {
+            System.out.println(trackToView.getTrackTitle() + "currently has no moves");
+        } else {
+            for (Move m : trackToView) {
+                int moveNumber = trackToView.getMoves().indexOf(m);
+                moveNumber++;
+                System.out.println(moveNumber + ". Name: " + m.getName() + " Reps: " + m.getReps());
+            }
+        }
+        showOptionsAfterViewTrackAndProcessSelectedOption(trackToView);
+
+    }
+
+    private void showOptionsAfterViewTrackAndProcessSelectedOption(Track trackToView) {
+        System.out.println("Please select how you would like to proceed:");
+        System.out.println("\t 1. Add a move");
+        System.out.println("\t 2. Delete a move");
+        System.out.println("\t 3. Return to tracklist");
+
+        int selectedOption = userInput.nextInt();
+
+        if (selectedOption == 1) {
+            processOption1(trackToView);
+
+        } else if (selectedOption == 2) {
+            processOption2(trackToView);
+        }
+        viewTracks();
+    }
+
+    private void processOption1(Track trackToView) {
+        System.out.println("Please enter the name of your new move:");
+        String newMoveName = userInput.next();
+        System.out.println("Please enter the number of reps of your new move:");
+        int newMoveReps = userInput.nextInt();
+        try {
+            trackToView.addMove(new Move(newMoveName, newMoveReps));
+            showOptionsAfterViewTrackAndProcessSelectedOption(trackToView);
+        } catch (UnrealisticRepsException e) {
+            System.out.println("You have entered an unrealistic number of reps. Please try again:");
+            printMovesInTrack(trackToView);
+        }
+    }
+
+    private void processOption2(Track trackToView) {
+        System.out.println("Please enter the number corresponding to the move you would like to delete:");
+        int moveToDeleteNum = userInput.nextInt();
+        moveToDeleteNum--;
+        Move moveToDelete = trackToView.getMoves().get(moveToDeleteNum);
+        String moveToDeleteName = moveToDelete.getName();
+        trackToView.getMoves().remove(moveToDeleteNum);
+        System.out.println("\"" + moveToDeleteName + "\"" + " has been successfully deleted from "
+                + trackToView.getTrackTitle());
+        showOptionsAfterViewTrackAndProcessSelectedOption(trackToView);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: deletes the track based on user entry
+    private void deleteTrack() {
+        System.out.println("Please enter the number corresponding to the track you would like to delete:");
+        int trackNumber = userInput.nextInt();
+        trackNumber--;
+        Track trackToDelete = workout.getTracks().get(trackNumber);
+        String trackToDeleteTrackTitle = trackToDelete.getTrackTitle();
+        workout.deleteTrack(trackToDelete);
+        System.out.println("\"" + trackToDeleteTrackTitle + "\""
+                + " has been successfully deleted from " + workout.getWorkoutTitle());
+        viewTracks();
     }
 
 
